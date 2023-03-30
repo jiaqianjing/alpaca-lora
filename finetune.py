6afc4aa03a8fe95108bef62c87ce53c9774953e9
jiaqianjing <jiaqianjing@gmail.com>
2023-03-30 22:57:40 +0800

    update instruction format

--- finetune.py
+++ finetune.py
@@ -73,26 +73,34 @@ train_data = train_val["train"]
 val_data = train_val["test"]
 
 
+# def generate_prompt(data_point):
+#     # sorry about the formatting disaster gotta move fast
+#     if data_point["input"]:
+#         return f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+# ### Instruction:
+# {data_point["instruction"]}
+
+# ### Input:
+# {data_point["input"]}
+
+# ### Response:
+# {data_point["output"]}"""
+#     else:
+#         return f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
+
+# ### Instruction:
+# {data_point["instruction"]}
+
+# ### Response:
+# {data_point["output"]}"""
+
 def generate_prompt(data_point):
-    # sorry about the formatting disaster gotta move fast
-    if data_point["input"]:
-        return f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
-
-### Instruction:
+    return f"""
+### Human:
 {data_point["instruction"]}
 
-### Input:
-{data_point["input"]}
-
-### Response:
-{data_point["output"]}"""
-    else:
-        return f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
-
-### Instruction:
-{data_point["instruction"]}
-
-### Response:
+### AI:
 {data_point["output"]}"""
 
 
